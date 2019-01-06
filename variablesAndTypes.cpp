d58c3f6acf4aaa8e227f0bf221ee11c54931815c
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-05 23:05:06 -0500

    added notes

---
+++ variablesAndTypes.cpp
@@ -0,0 +1,11 @@
+// boolean => true || false
+// int =>
+// char => -128 to 127
+// void =>
+// wchar_t =>
+// struct
+// class
+// signed 
+// unsigned
+// short
+// long => -2,147,483,648 to 2,147,483,647
