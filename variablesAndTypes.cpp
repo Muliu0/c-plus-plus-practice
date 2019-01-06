41b0f5d5955e6a3a6bc9c507f6b979a75d716445
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-06 07:15:20 -0500

    pushing new link

--- variablesAndTypes.cpp
+++ variablesAndTypes.cpp
@@ -9,3 +9,18 @@
 // unsigned
 // short
 // long => -2,147,483,648 to 2,147,483,647
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 3;
+	float b = 4.5;
+	double c = 5.25;
+	double sum;
+
+	sum = a + b + c;
+
+	cout << "The sum of a, b, and c is", sum << endl;
+	return 0;
+}
