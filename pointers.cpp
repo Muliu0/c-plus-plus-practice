6990d169aa2ed74e67b78641549764436897a44e
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-06 16:05:23 -0500

    added pointers example

--- pointers.cpp
+++ pointers.cpp
@@ -1,1 +1,21 @@
 //https://www.learn-cpp.org/en/Pointers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// declare n 
+	int n = 1;
+
+	// Create pointers for n
+	void* p1;
+	int* p2;
+
+	p1 = &n;
+	p2 = (int*)p1;
+	*p2 += 1;
+
+	cout << *p2 << endl;
+
+	return 0;
+}
